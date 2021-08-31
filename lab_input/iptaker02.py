6e2577f239128217f420c105203038beb605ecad
JasonTrespel <jtrespel13@gmail.com>
2021-08-31 18:53:25 +0000

    Lab 9, Collecting user input.

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+print("You told me the IPv4 address is:", user_input)
+
+user_input = input("Please enter the vendor name associated with the device:")
+print("You said the vendor name is:", user_input)
