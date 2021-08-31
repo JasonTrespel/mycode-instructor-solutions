321bd98c63061b869bb15983d01ce73a1ff2de32
JasonTrespel <jtrespel13@gmail.com>
2021-08-31 19:04:40 +0000

    Lab 10 working with lists.

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# comments are used to add descriptions to code and walk through a step-by-step process
+
+# creat a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# display the first item in the list
+print("The first item in the list (IP): " + my_list[0] )
+
+# display the second item in the list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# display the third itme in the list
+print("The last item in the list (state): " + my_list[2] )
+
