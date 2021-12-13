9b0bbf58978949762db9e584bd97fe8a18d13aa4
JasonTrespel <JasonTrespel@users.noreply.github.com>
2021-12-13 21:21:49 +0000

    Pushing lists.

---
+++ ex_input01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# setting variable
+color = "white"
+
+# will ask for favorite color 
+question = input("What is your favorite color?")
+
+# will print statement giving answer
+print(f"You said your favorite color is {question}!")
+
+# python gives a response
+print("That's a cool color! My favorite is white.")
+
+print("That's a cool color! My favorite is " + color)
--- py3_shebang.py
+++ py3_shebang.py
@@ -2,4 +2,6 @@
 print("ello!")
 print("Did you say, hello?")
 print("No, I said ello, but that\'s close enough.")
-
+print("that's vs that/'s")
+print("Here is my new statement.")
+print('No, I said ello, but that\'s close enough.') 
